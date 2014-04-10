1aef1d125d57354ee5880b3b92eea7acdb51a9d8
Arthur <arthur@aberman.(none)>
2014-04-09 20:43:02 -0400

    initial commit

---
+++ src/com/example/dss_game/Engine.java
@@ -0,0 +1,91 @@
+package com.example.dss_game;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.Environment;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceHolder.Callback;
+import android.view.SurfaceView;
+
+public class Engine extends SurfaceView implements Callback {
+	int x =0, y=0;
+	Bitmap room, monster, sword;
+	private String directory;
+	Paint paint;
+	public Engine(Context context, AttributeSet attrs) {
+		super(context);
+		getHolder().addCallback(this);
+		setFocusable(true);
+		setWillNotDraw(false);
+		paint = new Paint();
+		//room = BitmapFactory.decodeFile(System.getProperty("user.id")+"/res/drawable-hdpi/room.png");
+		System.out.println(Environment.getExternalStorageDirectory()+"/DSS-game/res/drawable-hdpi/room.png");
+	}
+	
+	public void init(String directory) {
+		this.directory = directory;
+		room = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.room), 1280, 720, false);
+		monster = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.monster), 240, 320, false);
+		sword = BitmapFactory.decodeResource(getResources(), R.drawable.sword);
+		System.out.println("Did something...");
+		new Thread() {
+			public void run() {
+				while (true) {
+					update();
+					try {
+						sleep(5);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}.start();
+	}
+	public void update() {
+	//	invalidate();
+	}
+	@Override
+	public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	protected void onDraw(Canvas canvas) {
+		//canvas.drawARGB(255, y % 255, x % 255, 255);
+		try {
+			canvas.drawBitmap(room, 0, 0, paint);
+			canvas.drawBitmap(monster, 500, 250, paint);
+			canvas.drawBitmap(sword, 750, 300, paint);
+		} catch (Exception e) {
+		//	e.printStackTrace();
+		}
+	}
+	public boolean onTouchEvent(MotionEvent event) {
+		x=(int)event.getX(0);
+		y=(int)event.getY(0);
+		return super.onTouchEvent(event);
+		
+	}
+}
---
+++ src/com/example/dss_game/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.dss_game;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.os.Build;
+
+public class MainActivity extends ActionBarActivity {
+	String directory;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		setContentView(R.layout.activity_main);
+
+
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+		PackageManager m = getPackageManager();
+		String s = getPackageName();
+		PackageInfo p = null;
+		try {
+			p = m.getPackageInfo(s, 0);
+			directory = p.applicationInfo.dataDir;
+
+		} catch (NameNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		final LinearLayout l = (LinearLayout) findViewById(R.id.container);
+		System.out.println(l);
+		System.out.println(l.getChildCount());
+		final Engine e = (Engine) l.getChildAt(0);
+				//(Engine) findViewById(R.id.gamewindow);
+		System.out.println(e);
+		e.init(directory);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container,
+					false);
+			return rootView;
+		}
+	}
+
+}
