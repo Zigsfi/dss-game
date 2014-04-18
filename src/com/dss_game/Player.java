45336a4a0533dc020675e7911347836bd4e9edc9
Arthur <arthur@aberman.(none)>
2014-04-17 21:23:20 -0400

    i'm dumb

--- src/com/dss_game/Player.java
+++ src/com/dss_game/Player.java
@@ -17,7 +17,7 @@ public class Player {
 	}
 
 	public void draw (Canvas c) {
-		c.drawBitmap(right.image(), (int)(1050 * Engine.scaleX), (int)(800+right.readiness()), engine.paint);
+		c.drawBitmap(right.image(), (int)(1050 * Engine.scaleX), (int)((800+right.readiness()) * Engine.scaleY), engine.paint);
 		c.drawBitmap(left.image(), (int)(400 * Engine.scaleX), (int)((600+left.readiness()) * Engine.scaleY), engine.paint);
 		c.drawBitmap(left.menu(engine.paint), (int)(200 * Engine.scaleX), (int)((340+left.readiness()) * Engine.scaleY), engine.paint);
 	}
