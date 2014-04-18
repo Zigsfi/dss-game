7794730c253c0ff693eae2b039d940c25029be1b
Will Hickey <whicke01@gmail.com>
2014-04-18 12:48:27 -0400

    added status to player and half of demon

--- src/com/dss_game/Player.java
+++ src/com/dss_game/Player.java
@@ -4,13 +4,27 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 
-public class Player {
+public class Player implements Stats{
 	Engine engine;
 	Weapon left, right;
+	int Hp;
+	int Mp;
+	int Str;
+	int Def;
+	int Dex;
+	int IQ /*Intellect*/;
+	
 	public Player(Engine e) {
 		engine = e;
 		right = new Sword(e);
 		left = new Shield(e);
+		Hp = 100;
+		Mp = 100;
+		Str = 10; /* + swordStats*/
+		Def = 10; /* + shiedStats*/
+		Dex = 10; /* + armorStats - shieldStats*/
+		IQ = 10; /* + rightStats*/
+		
 	}
 
 	public void update() {
@@ -31,5 +45,113 @@ public class Player {
 			right.tapped(x, y);
 		}
 
+	}
+
+	@Override
+	public int getHp() {
+		// TODO Auto-generated method stub
+		return Hp;
+	}
+
+	@Override
+	public void setHp(int curHp) {
+		Hp = curHp;
+		
+	}
+
+	@Override
+	public void changeHp(int addHp) {
+		Hp = Hp + addHp;
+		
+	}
+
+	@Override
+	public int getMp() {
+		// TODO Auto-generated method stub
+		return Mp;
+	}
+
+	@Override
+	public void setMp(int curMp) {
+		Mp = curMp;
+		
+	}
+
+	@Override
+	public void changeMp(int addMp) {
+		Mp = Mp + addMp;
+		
+	}
+
+	@Override
+	public int getStr() {
+		// TODO Auto-generated method stub
+		return Str;
+	}
+
+	@Override
+	public void setStr(int curStr) {
+		Str = curStr;
+		
+	}
+
+	@Override
+	public void changeStr(int addStr) {
+		Str = Str + addStr;
+		
+	}
+
+	@Override
+	public int getDef() {
+		// TODO Auto-generated method stub
+		return Def;
+	}
+
+	@Override
+	public void setDef(int curDef) {
+		Def = curDef;
+		
+	}
+
+	@Override
+	public void changeDef(int addDef) {
+		Def = Def + addDef;
+		
+	}
+
+	@Override
+	public int getDex() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setDex(int curDex) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void changeDex(int addDex) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getInt() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setInt(int curInt) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void changeInt(int addInt) {
+		// TODO Auto-generated method stub
+		
 	}
 }
