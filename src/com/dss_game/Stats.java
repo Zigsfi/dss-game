a60158d977d84219b4d4053162be76d70d5b4436
Will Hickey <whicke01@gmail.com>
2014-04-18 11:51:25 -0400

    added Intellect to Stats.java

--- src/com/dss_game/Stats.java
+++ src/com/dss_game/Stats.java
@@ -5,27 +5,32 @@ public interface Stats {
 	/*health*/
 	public int getHp();
 	public void setHp(int curHp);
-	public void changeHp(int addMe);
+	public void changeHp(int addHp);
 	
 	/*Magic*/
 	public int getMp();
 	public void setMp(int curMp);
-	public void changeMp(int addMe);
+	public void changeMp(int addMp);
 	
 	/*Attack/Strength*/
 	public int getStr();
 	public void setStr(int curStr);
-	public void changeStr(int addMe);
+	public void changeStr(int addStr);
 	
 	/*Defence*/
 	public int getDef();
 	public void setDef(int curDef);
-	public void changeDef(int addMe);
+	public void changeDef(int addDef);
 	
 	/*Evasion/Dex*/
 	public int getDex();
 	public void setDex(int curDex);
-	public void changeDex(int addMe);
+	public void changeDex(int addDex);
+	
+	/*intellect*/
+	public int getInt();
+	public void setInt(int curInt);
+	public void changeInt(int addInt);
 	
 	
 }
