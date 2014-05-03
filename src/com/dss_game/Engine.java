46fb7452b30b21e3dec9f9b8282cdf329fc57e2e
Arthur <arthur@aberman.(none)>
2014-05-03 12:53:51 -0400

    fixed weird menu rendering bug

--- src/com/dss_game/Engine.java
+++ src/com/dss_game/Engine.java
@@ -110,7 +110,7 @@ public class Engine extends SurfaceView implements Callback, OnGestureListener {
 
 			c.drawBitmap(dungeon.render(), dungeonX, dungeonY, paint);
 			c.drawBitmap(dungeon.curRoom.getMenu().render((dungeon.curRoom == startRoom ? 800 : 400), 400, paint), 0, 0, paint);
-			c.drawBitmap(player.menu.render((int)(800 * scaleX),(int)( player.menuHeight * scaleY), paint), 1920*scaleX - 800 * scaleY, 0, paint);
+			c.drawBitmap(player.menu.render((int)(800 * scaleX),(int)( player.menuHeight * scaleY), paint), 1920*scaleX - (800 * scaleX), 0, paint);
 			surfaceholder.unlockCanvasAndPost(c);
 		}
 	}
