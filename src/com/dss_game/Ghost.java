723a9e7ebfadd783385f904f599d456cb4b8d04d
Will Hickey <whicke01@gmail.com>
2014-05-04 14:06:41 -0400

    ghost now fades in and out

--- src/com/dss_game/Ghost.java
+++ src/com/dss_game/Ghost.java
@@ -33,6 +33,60 @@ public class Ghost extends Enemy{
 	}
 	
 	@Override
+	public void update() {
+		
+		if(sAttack <= 0){
+			hardHit();
+			//sAttack = 700;
+			readiness = 200;
+		}else if(readiness <= 0){
+			int damage = Str - Engine.player.getDef();
+			if(damage <= 0) {
+				damage = 1;
+			}
+				//check dex hit chance
+				int pdex = Engine.player.getDex();
+				int mydex = ((int)(Math.random() * pdex)) + Dex;
+				Dex = 10;
+				if (pdex - mydex >= 0) {
+					Engine.message = "Enemy missed";
+					image = missImage;
+					Timer def_T = new Timer();
+					def_T.schedule(new TimerTask() {
+						@Override
+						public void run() {
+							image = normImage;
+							Engine.message = "";
+							return;
+						}
+					}, 500);
+					readiness = 200;
+				}else {
+					//attack the player
+					Engine.player.changeHp(-1 * damage);
+					Engine.message = "Enemy attacks for " + damage;
+					image = hitImage;
+					Timer def_T = new Timer();
+					def_T.schedule(new TimerTask() {
+						@Override
+						public void run() {
+							image = normImage;
+							Engine.message = "";
+							return;
+						}
+					}, 750);
+					readiness = 200;
+				}
+		}
+		readiness--;
+		sAttack--;
+		if (Hp <= 0){
+			death();
+		}
+		
+	}
+	
+	@Override
 	public void hardHit() {
 		int fadeDex = (int) Engine.player.getDex() / 2;
 		
@@ -40,15 +94,14 @@ public class Ghost extends Enemy{
 		changeDex(fadeDex);
 		Engine.message = "Ghost fades: harder to hit ";
 		image = Bitmap.createScaledBitmap(
-				BitmapFactory.decodeResource(this.engine.getResources(), R.drawable.ghost),(int)( 480 * Engine.scaleX), (int)(720 * Engine.scaleY), false);;
+				BitmapFactory.decodeResource(this.engine.getResources(), R.drawable.ghost),(int)( 480 * Engine.scaleX), (int)(720 * Engine.scaleY), false);
 		Timer def_T = new Timer();
 		def_T.schedule(new TimerTask() {
 			@Override
 			public void run() {
-				image = normImage;
-				int web = (int) Engine.player.getDex() * 2;
-				Engine.player.changeDex(web);
-				Engine.message = "Cleared the web: Dex up " + web;
+				image = normImage;;
+				Engine.player.setDex(10);
+				Engine.message = "Ghost reappeared.";
 				return;
 			}
 		}, 4000);
